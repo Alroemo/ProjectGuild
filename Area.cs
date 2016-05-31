946aefa4253e48a616dcc4bc8500a36b2daf2665
nattokun <avroeum@gmail.com>
2016-05-31 15:09:11 -0400

    Update Area.cs

--- Area.cs
+++ Area.cs
@@ -18,6 +18,43 @@ namespace ProjectGuild
 {
   class Area
   {
+    char [][] area;
+    int [][2]rooms;
+    int width;
+    int height;
+    //the number of rectangled rooms in the area
+    int areaCount;
+    //the level of mazes and halls to add
+    int complexity;
+    
+    public Area(int _width, int _height, int _areaCount, int _complexity)
+    {
+      width = _width;
+      height = _height;
+      areaCount = _areaCount;
+      complexity = _complexity;
+    }
+    
+    void initalizeArea()
+    {
+      for(int row = 0; row < height; row++)
+      {
+        for(int col = 0; col < width; col++)
+        {
+          area[i][j] = '.';
+        }
+      }
+      
+      for (int roomCount = 0; roomCount < areaCount; roomCount++)
+      {
+        
+      }
+    }
+    
+    public void makeArea()
+    {
+      
+    }
     
   }
 }
