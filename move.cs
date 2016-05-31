9837b73fd2e96436cc3ca7245e4b4f5d6c741bfb
nattokun <avroeum@gmail.com>
2016-05-31 09:13:01 -0400

    Update move.cs

--- move.cs
+++ move.cs
@@ -1,1 +1,47 @@
+#region File Description
+//-----------------------------------------------------------------------------
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
 
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectGuild
+{
+  class move
+  {
+    string name;
+    string type;
+    int actionDealt;
+    int actionAmmount;
+    string element;
+    
+    public move(string _name, string _type, int _actionDealt, int _actionAmmount, string _element)
+    {
+      name = _name;
+      type = _type;
+      actionDealt = _actionDealt;
+      actionAmmount = _actionAmmount;
+      element = _element;
+    }
+    
+    /** functions to get move information*/
+    public string getName()
+    { return name; }
+    public string getType()
+    { return type; }
+    public int getActionDealt()
+    { return actionDealt; }
+    public int getActionAmmount()
+    { return actionAmmount; }
+    public string getElement()
+    { return element; }
+    
+  }
+}
