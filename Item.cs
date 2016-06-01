08503b66158da767484475b0686bace615e08237
nattokun <avroeum@gmail.com>
2016-05-31 20:10:53 -0400

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -1,26 +1,15 @@
-
-
-#region File Description
-//-----------------------------------------------------------------------------
-//
-// Microsoft XNA Community Game Platform
-// Copyright (C) Microsoft Corporation. All rights reserved.
-//-----------------------------------------------------------------------------
-#endregion
-
 using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ProjectGuild
 {
-  class Item
-  {
-    string name;
-    string description;
-    int status;
-    
-  }
+    class Item
+    {
+        string name;
+        string description;
+        int status;
+    }
 }
