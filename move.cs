12242a615c9f97b7565516eb75aaacd2599f461b
nattokun <avroeum@gmail.com>
2016-05-31 20:10:17 -0400

    Update move.cs

--- move.cs
+++ move.cs
@@ -1,47 +1,38 @@
-#region File Description
-//-----------------------------------------------------------------------------
-//
-// Microsoft XNA Community Game Platform
-// Copyright (C) Microsoft Corporation. All rights reserved.
-//-----------------------------------------------------------------------------
-#endregion
-
 using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ProjectGuild
 {
-  class move
-  {
-    string name;
-    string type;
-    int actionDealt;
-    int actionAmmount;
-    string element;
-    
-    public move(string _name, string _type, int _actionDealt, int _actionAmmount, string _element)
+    class Move
     {
-      name = _name;
-      type = _type;
-      actionDealt = _actionDealt;
-      actionAmmount = _actionAmmount;
-      element = _element;
+        string name;
+        string type;
+        int actionDealt;
+        int actionAmmount;
+        string element;
+
+        public Move(string _name, string _type, int _actionDealt, int _actionAmmount, string _element)
+        {
+            name = _name;
+            type = _type;
+            actionDealt = _actionDealt;
+            actionAmmount = _actionAmmount;
+            element = _element;
+        }
+
+        /** functions to get move information*/
+        public string getName()
+        { return name; }
+        public string getType()
+        { return type; }
+        public int getActionDealt()
+        { return actionDealt; }
+        public int getActionAmmount()
+        { return actionAmmount; }
+        public string getElement()
+        { return element; }
     }
-    
-    /** functions to get move information*/
-    public string getName()
-    { return name; }
-    public string getType()
-    { return type; }
-    public int getActionDealt()
-    { return actionDealt; }
-    public int getActionAmmount()
-    { return actionAmmount; }
-    public string getElement()
-    { return element; }
-    
-  }
 }
