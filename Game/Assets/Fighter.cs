27aa2627189863b5863ba22a06b7601e11c34757
nattokun <avroeum@gmail.com>
2016-06-01 12:01:12 -0400

    Update Fighter.cs

--- Game/Assets/Fighter.cs
+++ Game/Assets/Fighter.cs
@@ -10,11 +10,13 @@ namespace ProjectGuild
     {
 
         /** Array of all characters in use*/
+        Character currentCharacter;
         Character[] currentParty;
         int partyCount;
 
-        public Fighter(Character[] _currentParty, int _partyCount)
+        public Fighter(Character _currentCharacter, Character[] _currentParty, int _partyCount)
         {
+            currentCharacter = _currentCharacter;
             partyCount = _partyCount;
             currentParty = _currentParty;
         }
@@ -22,6 +24,8 @@ namespace ProjectGuild
         #region getFunctions
         public Character[] getPartyCharacters()
         { return currentParty; }
+        public Character getCharacter()
+        { return currentCharacter; }
         public Character getPartyCharacter(int charNum)
         { return currentParty[charNum]; }
         #endregion
@@ -29,8 +33,13 @@ namespace ProjectGuild
         public void initalize()
         {
             partyCount = 0;
+            currentCharacter = currentParty[0];
         }
-
+        
+        public void changeCurrentCharacter(int charNum)
+        {
+            currentCharacter = currentParty[charNum];
+        }
         public void addCharacters(Character newCharacter)
         {
             partyCount += 1;
