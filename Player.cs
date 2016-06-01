582f400465bd417e87f2823404a409e750834bae
nattokun <avroeum@gmail.com>
2016-05-31 20:11:39 -0400

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,35 +1,47 @@
-
-
-#region File Description
-//-----------------------------------------------------------------------------
-//
-// Microsoft XNA Community Game Platform
-// Copyright (C) Microsoft Corporation. All rights reserved.
-//-----------------------------------------------------------------------------
-#endregion
-
 using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ProjectGuild
 {
-  class Player
-  {
-    /** Array of all characters*/
-    character [200] characters;
-    /** Variable to set the maximum number of characters the player can have based on base size.*/
-    int maxCharacters;
-    int currentOwnedCharacters;
-    /** Array of all characters in use*/
-    character [4] currentParty;
-    
-    public Player(Character [] _characters, int _maxCharacters, int _curentOwnedCharacters, character [4] _currentParty)
+    class Player : Fighter
     {
-      
+        /** Array of all characters*/
+        Character[] characters;
+        /** Variable to set the maximum number of characters the player can have based on base size.*/
+        int maxCharacters;
+        int currentOwnedCharactersAmmount;
+
+
+        public Player(Character[] _characters, Character[] _currentParty, int _partyCount, int _maxCharacters, int _curentOwnedCharactersAmmount) : base (_currentParty, _partyCount)
+        {
+            characters = _characters;
+            maxCharacters = _maxCharacters;
+            currentOwnedCharactersAmmount = _curentOwnedCharactersAmmount;
+        }
+
+        #region getFunctions
+        public void initalize()
+        {
+            currentOwnedCharactersAmmount = 0;
+        }
+
+        public Character[] getCharacters()
+        { return characters; }
+        public Character getCharacter(int charNum)
+        { return characters[charNum]; }
+        public int getMaxCharacters()
+        { return maxCharacters; }
+        public int getCurrentCharactersAmmount()
+        { return currentOwnedCharactersAmmount; }
+        #endregion
+
+        public void addCharacters(Character newCharacter)
+        {
+            currentOwnedCharactersAmmount += 1;
+            characters[currentOwnedCharactersAmmount] = newCharacter;
+        }
     }
-    
-  }
 }
