70aa33b3c03092b62abe93eb397a448f0041c064
nattokun <avroeum@gmail.com>
2016-06-03 09:03:38 -0400

    Update Battle.cs

--- Game/Gamemode/Battle.cs
+++ Game/Gamemode/Battle.cs
@@ -9,8 +9,10 @@ namespace ProjectGuild
 {
     class Battle
     {
+        Random Rand;
+        
         Player player;
-        Fighter opponent;
+        Opponent opponent;
         Character currentPlayerCharacter;
         Character currentEnemyCharacter;
         public Battle(Player _player, Fighter _opponent)
@@ -101,18 +103,47 @@ namespace ProjectGuild
                 first = false;
         }
         
-        public void makeChoice()
+        public void makeChoice(int choiceTier, int choice)
         {
             string [] main = {"Attack", "Defend", "Item", "Switch", "Flee"};
-            string [] attack = { "move1", "move2", "move3", "move4"};
-            string [] charSwitch = {"char1", "char2","char3", "char4"};
+            Move [] attack = { "move1", "move2", "move3", "move4"};
+            Character [] charSwitch = {"char1", "char2","char3", "char4"};
             for(int i = 0; i < 4; i++)
             {
-                attack[i] = currentPlayerCharacter.getMove[i].getName(); 
-                charSwitch[i] = player.getCharacter[i].getName();
+                attack[i] = currentPlayerCharacter.getMove[i]; 
+                charSwitch[i] = player.getCharacter[i];
             }
             
+            switch(choice):
+                //attack
+                case 1:
+                    makeChoice(2,0);
+                //defend
+                case 2:
+                    player.getCurrentCharacter.defend();
+                case 3:
+                    player.openItemList();
+                case 4:
+                    player.openCurrentParty();
+                case 5:
+                    this.flee;
+        }
         
+        public void playerFlee()
+        {
+            int fleeChance = Rand.next(0, 100);
+            int flee = Rand.next(0,100);
+            
+            if(opponent.isBoss())
+                fleeChance = 0;
+                
+            if(flee < fleeChance)
+                this.endGame();
+        }
+        
+        public void endBattle(int xp, Item rewardItem, int itemChance)
+        {
+            
         }
     }
 }
