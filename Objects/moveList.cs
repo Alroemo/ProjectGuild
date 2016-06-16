1667cfe639c16c0f79584a502e1544e7ad4543c2
nattokun <avroeum@gmail.com>
2016-06-16 10:06:38 -0400

    Create moveList.cs

---
+++ Objects/moveList.cs
@@ -0,0 +1,28 @@
+#region File Description
+//-----------------------------------------------------------------------------
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectGuild
+{
+  class moveList
+  {
+    public Move [] moves = {
+      
+    };
+    
+    public Move getMove(int num)
+    {
+      return moves[num];
+    }
+  }
+}
