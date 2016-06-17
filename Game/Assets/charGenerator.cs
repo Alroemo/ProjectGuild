25d8523095ace1defa6dcf4c9267a42c9c128c81
nattokun <avroeum@gmail.com>
2016-06-17 09:27:32 -0400

    Create charGenerator.cs

---
+++ Game/Assets/charGenerator.cs
@@ -0,0 +1,21 @@
+#region File Description
+//-----------------------------------------------------------------------------
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectGuild
+{
+  class /*Insert class name here*/
+  {
+    
+  }
+}
