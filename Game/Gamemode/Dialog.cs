76d6818c5bf7f5a3b605a1e26dff8bf5fab71989
nattokun <avroeum@gmail.com>
2016-06-17 09:22:23 -0400

    Update Dialog.cs

--- Game/Gamemode/Dialog.cs
+++ Game/Gamemode/Dialog.cs
@@ -18,21 +18,23 @@ namespace ProjectGuild
   {
     SpriteFont spriteFont; 
     Rectangle dialogBox;
-    string dialog
-    Vector2 wordPosition;
+    string [] dialog
+    Vector2 [] wordPosition;
     
-    public Dialog(_spriteFont, string dialog)
+    public Dialog(_spriteFont, string [] _dialog)
     {
       spriteFont = _spriteFont;
       dialog = _dialog;
       dialogBox = new Rectangle(10, 580, 760, 200);
-      wordPosition = new vector2(15, 585);
+      for(int i = 0; i < dialog.length; i++)
+        wordPosition[i] = new vector2(15 + (i * 18), 585);
     }
     
     publiv void draw(SpriteBatch spriteBatch)
     {
       spriteBatch.draw((Content.load<Texture2D>("dialogBox")), dialogBox, Color.White);
-      spriteBatch.drawString(spriteFont, dialog, wordPosition, Color.Yellow);
+      for(int i = 0; i < dialog.length;  i++)
+        spriteBatch.drawString(spriteFont, dialog[i], wordPosition[i], Color.Yellow);
     }
     
   }
