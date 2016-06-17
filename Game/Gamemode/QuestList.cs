4b5545c45ca4201164e47354723a207911aa655f
nattokun <avroeum@gmail.com>
2016-06-17 10:55:38 -0400

    Create QuestList.cs

---
+++ Game/Gamemode/QuestList.cs
@@ -0,0 +1,22 @@
+
+#region File Description
+//-----------------------------------------------------------------------------
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectGuild
+{
+  class QuestList
+  {
+    
+  }
+}
